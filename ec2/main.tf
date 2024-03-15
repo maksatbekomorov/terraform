0a8c12f5ec4d047d64063877f65a31e9070b3a3a
maksatbekomorov <151109926+maksatbekomorov@users.noreply.github.com>
2024-03-15 10:23:24 +0600

    update header and ami and subnet

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # Имя корзины, которое определили в самом начале
-    bucket = "terraform-state-bucket-chyngyz"
+    bucket = "terraform-state-bucket-maksat"
     # Путь к файлу состояния Terraform
     key = "global/s3/terraform-ec2.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
     
     # Имя таблицы в DynamoDB, которое определили в самом начале
     dynamodb_table = "terraform-state-locks"
@@ -19,27 +19,27 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "from-terraform-ubuntu" {
   count = 1
-  ami = "ami-0e0cbc26960fa5f7b"
+  ami = "ami-0b98a32b1c5e0d105"
   instance_type = var.ec2-type
-  subnet_id = "subnet-0b722ffeafb005890"
+  subnet_id = "subnet-048c2c7139defb91e"
   tags = {
-    Name = "from-terraform"
-    role = "nginx"
+    Name = "from-k8s"
+    role = "k8s"
   }
 }
 
-resource "aws_instance" "from-terraform-al" {
-  count = 1
-  ami = "ami-025a6a5beb74db87b"
-  instance_type = var.ec2-type
-  subnet_id = "subnet-0c621a57263c94961"
-  tags = {
-    Name = "from-terraform"
-    role = "nginx"
-  }
-}
+#resource "aws_instance" "from-terraform-al" {
+ # count = 1
+#  ami = "ami-025a6a5beb74db87b"
+#  instance_type = var.ec2-type
+#  subnet_id = "subnet-0c621a57263c94961"
+#  tags = {
+#    Name = "from-terraform"
+#    role = "nginx"
+#  }
+# }
